613ee26edd5f7a85d280ffa8b78a2d7bf1a01c65
chiay <chiay@oregonstate.edu>
2021-09-22 15:18:35 -0700

    fix import

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import 'react-virtualized/styles.css';
 import '@fontsource/roboto';
 import App from './App';
 
